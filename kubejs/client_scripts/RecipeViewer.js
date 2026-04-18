3a3d79358b1c78feb50818c2b10c16eab51a0f32
Pietro Lopes <97140255+pietro-lopes@users.noreply.github.com>
2026-04-18 08:36:27 -0300

    feat: add regional pika stars to jei

--- kubejs/client_scripts/RecipeViewer.js
+++ kubejs/client_scripts/RecipeViewer.js
@@ -101,5 +101,9 @@ RecipeViewerEvents.addInformation('fluid', allthemods => {
   ])
 })
 
+RecipeViewerEvents.registerSubtypes("item", event => {
+  event.useComponents("allthemons:pika_star",["allthemons:region"])
+})
+
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
 // As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
