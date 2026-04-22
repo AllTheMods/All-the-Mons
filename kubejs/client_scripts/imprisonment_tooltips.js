01d9274881d5218340c2df293e8aa5a608b9bcdc
EjahDev <116228748+ejahdev@users.noreply.github.com>
2026-04-21 23:52:24 -0700

    remove mob and health lines and add gender
    Removed duplicate tooltip line and added gender extraction from raw data.

--- kubejs/client_scripts/imprisonment_tooltips.js
+++ kubejs/client_scripts/imprisonment_tooltips.js
@@ -1,5 +1,7 @@
 ItemEvents.modifyTooltips(event => {
     event.modify('industrialforegoing:mob_imprisonment_tool', tooltip => {
+        tooltip.removeLine(1)
+        tooltip.removeLine(1)
         tooltip.dynamic('cobblemon_species')
     })
 })
@@ -17,6 +19,9 @@ ItemEvents.dynamicTooltips('cobblemon_species', event => {
     var shinyMatch = raw.match(/Shiny:(\w+)/)
     var isShiny = shinyMatch && shinyMatch[1] === '1b'
 
+    var gender = raw.match(/Gender:"([^"]+)"/)
+
     event.add(Text.of('§6Captured: §f' + formatted))
+    if (gender) event.add(Text.of('§7Gender: §f' + gender[1].charAt(0) + gender[1].slice(1).toLowerCase()))
     if (isShiny) event.add(Text.of('§e★ SHINY ★'))
 })
