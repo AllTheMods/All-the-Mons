62164883ab75fb94491da2daa8a72a979e341c9d
pietro-lopes <97140255+pietro-lopes@users.noreply.github.com>
2026-07-28 06:34:22 -0300

    feat(cobblemon): implement strippables via neoforge datamaps
    Closes #688

---
+++ kubejs/server_scripts/mods/Cobblemon/DataMaps.js
@@ -0,0 +1,27 @@
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
+
+ServerEvents.generateData("after_mods", allthemods => {
+    allthemods.json("neoforge:data_maps/block/strippables.json", {
+        "values": {
+            "cobblemon:apricorn_wood": {
+              "stripped_block": "cobblemon:stripped_apricorn_wood"
+            },
+            "cobblemon:apricorn_log": {
+              "stripped_block": "cobblemon:stripped_apricorn_log"
+            },
+            "cobblemon:saccharine_log": {
+              "stripped_block": "cobblemon:stripped_saccharine_log"
+            },
+            "cobblemon:saccharine_wood": {
+              "stripped_block": "cobblemon:stripped_saccharine_wood"
+            },
+            "cobblemon:saccharine_log_slathered": {
+              "stripped_block": "cobblemon:stripped_saccharine_log"
+            }
+        }
+    })
+})
+
+// This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods.
+// As all AllTheMods packs are licensed under All Rights Reserved, this file is not allowed to be used in any public packs not released by the AllTheMods Team, without explicit permission.
