f81843dc404970e9f64b99dc94c8ac19423284e4
Jais Djurhuus-Kempel <jdk@jdkdigital.cy>
2026-09-03 04:13:57 +0300

    Fixes #670

--- kubejs/server_scripts/mods/Create Enchantment Industry/recipes.js
+++ kubejs/server_scripts/mods/Create Enchantment Industry/recipes.js
@@ -10,6 +10,62 @@ ServerEvents.recipes(allthemods => {
       G: `apotheosis:god_fused_pearl`
     })
     .id("create_enchantment_industry:crafting/infuser");
+
+  const affixTemplates = [
+    {
+      name: "brass",
+      base: "create:copper_sheet",
+      steps: [
+        { type: "create:deploying", item: "minecraft:paper" },
+        { type: "create:deploying", item: "apotheosis:mysterious_scrap_metal" },
+        { type: "create:pressing" }
+      ]
+    },
+    {
+      name: "crystal",
+      base: "create:brass_sheet",
+      steps: [
+        { type: "create:deploying", item: "minecraft:paper" },
+        { type: "create:filling", fluid: "create_enchantment_industry:crystal_essence", amount: 50 },
+        { type: "create:deploying", item: "minecraft:amethyst_shard" },
+        { type: "create:pressing" }
+      ]
+    },
+    {
+      name: "apotheotic",
+      base: "create:sturdy_sheet",
+      steps: [
+        { type: "create:deploying", item: "minecraft:paper" },
+        { type: "create:filling", fluid: "create_enchantment_industry:apotheotic_essence", amount: 50 },
+        { type: "create:deploying", item: "apotheosis:godforged_pearl" },
+        { type: "create:pressing" }
+      ]
+    }
+  ];
+
+  affixTemplates.forEach((template) => {
+    const incomplete = `create_enchantment_industry:incomplete_${template.name}_affix_template`;
+
+    allthemods.remove({
+      id: `create_enchantment_industry:sequenced_assembly/${template.name}_affix_template`
+    });
+
+    allthemods
+      .custom({
+        type: "create:sequenced_assembly",
+        ingredient: { item: template.base },
+        results: [{ id: `create_enchantment_industry:${template.name}_affix_template` }],
+        transitional_item: { id: incomplete },
+        sequence: template.steps.map((step) => {
+          const ingredients = [{ item: incomplete }];
+          if (step.item) ingredients.push({ item: step.item });
+          if (step.fluid)
+            ingredients.push({ type: "neoforge:single", amount: step.amount, fluid: step.fluid });
+          return { type: step.type, ingredients: ingredients, results: [{ id: incomplete }] };
+        })
+      })
+      .id(`allthemods:sequenced_assembly/${template.name}_affix_template`);
+  });
 });
 
 // This File has been authored by AllTheMods Staff, or a Community contributor for use in AllTheMods - AllTheMods 10.
